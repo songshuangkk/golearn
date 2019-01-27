1a745429a9b68f41386be8e2f2d021cbd36f2081
songshuangkk <songshuangkk@gmail.com>
2019-01-27 22:30:58 +0800

    数组的基本使用

--- array/demo.go
+++ array/demo.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func pointerCopy() {
+	var array1 [3]*string
+
+	array2 := [3]*string{new(string), new(string), new(string)}
+
+	*array2[0] = "Red"
+	*array2[1] = "Blue"
+	*array2[2] = "Green"
+
+	// 由于使用的是一个指针，复制之后，两个数组的元素指向的都是同一个值
+	array1 = array2
+}
+
 func main() {
 	var arr1 [5]int
 	fmt.Printf("arr1 size = %d, %d", len(arr1), arr1)
@@ -21,4 +34,11 @@ func main() {
 	arr4 := []int{1, 2, 3, 4, 5}
 	fmt.Printf("arr4 size = %d, %d", len(arr4), arr4)
 	fmt.Println()
+
+	// 数组只有在相同的类型和长度之间才能进行一个复制操作。
+	var arr5 [5]int
+	// 这里是不能进行直接复制的，因为这个arr4的长度是需要运行时推算出来的，长度不明确
+	//arr5 = arr4
+	arr5 = arr1
+	fmt.Println(arr5)
 }
