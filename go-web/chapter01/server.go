811c894ec43c10c52b25ffeb2244d43affae2c50
songshuangkk <songshuangkk@gmail.com>
2019-02-14 22:02:48 +0800

    go web

---
+++ go-web/chapter01/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	_, _ = fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	_ = http.ListenAndServe(":8080", nil)
+}
---
+++ go-web/chapter02/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads()
+	if err == nil {
+		_ = templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static", http.StripPrefix("/static", files))
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	_ = server.ListenAndServe()
+}
